453caabcb1587056a18b999b5d9089e80ba80cdb
Susane <147738127+Su-Zhou@users.noreply.github.com>
2023-12-05 23:51:15 -0700

    Footer, hover button and incrementing btn (JS)

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 console.log('Hello, World!');
+
+
+document.addEventListener ("DOMContentLoaded", function() { /*footer*/
+    var date = new Date().getFullYear();
+    document.getElementById("year").innerHTML = date;
+});
+
+
+function AlertFunction() {
+    alert('Just Kidding! Use the functioning back to top arrow that has been following your journey down this website!')
+}
+
+function hoverbuttonON() {
+    document.getElementById("backtoTOP").innerHTML = "GOTCCHA!"
+}
+function hoverbuttonOFF() {
+    document.getElementById("backtoTOP").innerHTML = "Back to Top"
+}
+
+
+
+function totalClick(click) {
+    const totalClicks = document.getElementById('totalClicks');
+    const sumvalue = parseInt(totalClicks.innerText) + click;
+
+    // Avoiding negatives
+    if (sumvalue < 0) {
+        totalClicks.innerText = 0;
+    } else {
+        totalClicks.innerText = sumvalue;
+    }
+
+    // Reset value
+    if (click === 0) {
+        totalClicks.innerText = 0;
+    }
+    console.log(totalClicks.innerText);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
